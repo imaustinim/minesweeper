32bd8a7bba36e4f32753b5ea41636c5e726b2a2e
Austin Im <imaustinim@gmail.com>
2021-02-22 13:57:01 -0500

    added settings

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 class Board {
     constructor() {
-        this.board = document.querySelector("#gameboard");;
-        this.width = document.querySelector("#board-width");;
-        this.height = document.querySelector("#board-height");;
-        this.bombs = document.querySelector("#bombs-amount");;
+        this.board = document.querySelector("#gameboard");
+        this.width = document.querySelector("#board-width").value;
+        this.height = document.querySelector("#board-height").value;
+        this.bombs = document.querySelector("#board-bombs").value;
+        this.gameType = document.querySelector("#gameType").value;
         this.boardArray = [];
-        this.gameType = document.querySelector("#gameType");;
         this.activeCells = [];
         this.bombCounter = document.querySelector("#bomb-counter");
         this.minutes = document.querySelector("#minutes");
@@ -22,18 +22,18 @@ class Board {
     }
 
     createBoard() {
-        for (let i = 0; i < this.width.value; i++) {
+        for (let i = 0; i < this.width; i++) {
             const row = document.createElement("div");
             const rowArray = [];
             row.className = "row";
             this.board.appendChild(row);
-            for (let j = 0; j < this.height.value; j++) {
+            for (let j = 0; j < this.height; j++) {
                 const div = document.createElement("div");
                 div.className = "cell";
                 row.appendChild(div);
                 const cell = document.createElement("img");
                 div.appendChild(cell);
-                cell.src = `img/${this.gameType.value}/0.png`;
+                cell.src = `img/${this.gameType}/0.png`;
                 cell.className = "cell-img";
                 cell.classList.add("active-cell");
                 rowArray.push({
@@ -50,8 +50,8 @@ class Board {
 
     populateBoard() {
         const createBombs = () => {
-            let x = Math.round(Math.random()*(this.width.value-1));
-            let y = Math.round(Math.random()*(this.height.value-1));
+            let x = Math.round(Math.random()*(this.width-1));
+            let y = Math.round(Math.random()*(this.height-1));
             const cell = this.boardArray[x][y];
             if (cell.value === 0) {
                 cell.value = -2;
@@ -61,13 +61,13 @@ class Board {
         }
             
         const addNumbers = (e) => {
-            for (let x = 0; x < this.width.value; x++) {
-                for (let y = 0; y < this.height.value; y++) {
+            for (let x = 0; x < this.width; x++) {
+                for (let y = 0; y < this.height; y++) {
                     const cell = this.boardArray[x][y];
                     if (cell.value === -2) {
                         for (let i = x-1; i <= x+1; i++) {
                             for (let j = y-1; j <= y+1; j++) {
-                                if (i >= this.width.value || j >= this.height.value || i < 0 || j < 0) continue;
+                                if (i >= this.width || j >= this.height || i < 0 || j < 0) continue;
                                 const adjCell = this.boardArray[i][j];
                                 if (adjCell.value >= 0) {
                                     adjCell.value += 1;
@@ -81,7 +81,6 @@ class Board {
 
         function setTime() {
             ++totalSeconds;
-        
             this.seconds.innerHTML = pad(totalSeconds % 60);
             this.minutes.innerHTML = pad(parseInt(totalSeconds / 60));
         
@@ -94,12 +93,9 @@ class Board {
                 }
             }
         }
-
-
         
-        
-        let num1 = this.bombs.value/100*this.width.value*this.height.value;
-        let num2 = this.width.value*this.height.value-1;
+        let num1 = this.bombs/100*this.width*this.height;
+        let num2 = this.width*this.height-1;
         let numBombs = Math.ceil(Math.min(num1, num2));
         this.bombCounter.innerHTML = numBombs;
         while (numBombs > 0) {
@@ -127,7 +123,7 @@ class Board {
             cell.value = -1;
             for (let i = x-1; i <= x+1; i++) {
                 for (let j = y-1; j <= y+1; j++) {
-                    if (i >= this.width.value || j >= this.width.value || i < 0 || j < 0) continue;
+                    if (i >= this.width || j >= this.height || i < 0 || j < 0) continue;
                     const adjCell = this.boardArray[i][j];
                     if (adjCell === cell) continue;
                     if (adjCell.value === 0) {
@@ -149,7 +145,7 @@ class Board {
             //         cell.element.style.color = "red";
             //     }
             // } 
-            cell.element.src = `img/${this.gameType.value}/${cell.value}.png`;
+            cell.element.src = `img/${this.gameType}/${cell.value}.png`;
             cell.element.classList.remove("active-cell");
             this.activeCells.splice(index, 1);
         }
@@ -168,7 +164,7 @@ class Board {
                         const t = cell.value;
                         cell.value = cell.temp_value;
                         cell.temp_value = t;
-                        cell.element.src = `img/${this.gameType.value}/${cell.value}.png`;
+                        cell.element.src = `img/${this.gameType}/${cell.value}.png`;
                         cell.element.classList.remove("active-cell");
                         this.bombCounter.innerHTML = parseInt(this.bombCounter.innerHTML)-1;
                     }
@@ -176,7 +172,7 @@ class Board {
                     const t = cell.value;
                     cell.value = cell.temp_value;
                     cell.temp_value = t;
-                    cell.element.src = `img/${this.gameType.value}/0.png`;
+                    cell.element.src = `img/${this.gameType}/0.png`;
                     cell.element.classList.add("active-cell");
                     this.bombCounter.innerHTML = parseInt(this.bombCounter.innerHTML)+1;
                 }
@@ -210,25 +206,61 @@ class Board {
         }
     }
 
-    saveSettings() {
-
+    getChanges() {
+        this.width = document.querySelector("#board-width").value;
+        this.height = document.querySelector("#board-height").value;
+        this.bombs = document.querySelector("#board-bombs").value;
+        this.gameType = document.querySelector("#gameType").value;
     }
 }
 
 // __main__
 const board = new Board();
 const newGame = document.querySelector("#start-button");
-newGame.addEventListener("click", () => {
+newGame.addEventListener("click", (e) => {
+    e.preventDefault();
     board.deleteboard();
+    board.getChanges();
     board.createBoard();
     board.populateBoard();
     board.activeCells.forEach(item => board.clickEventListener(item));
     board.activeCells.forEach(item => board.rightClickEventListener(item));
 });
 
-const settings = document.querySelector("#settings");
-settings.addEventListener("click", () => {
+const settingsBtn = document.querySelector("#settings-button");
+const modalBg = document.querySelector(".modal-bg");
+const saveBtn = document.querySelector("#save-changes");
+
+settingsBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    modalBg.classList.add("modal-bg-active");
 })
+
+saveBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    board.getChanges();
+    modalBg.classList.remove("modal-bg-active");
+})
+
+const width = document.querySelector("#board-width");
+const height = document.querySelector("#board-height");
+const bombs = document.querySelector("#board-bombs");
+const gameType = document.querySelector("#gameType")
+const settings = [width, height, bombs, gameType];
+settings.forEach(item => changeSettings(item));
+
+function changeSettings(item) {
+    item.addEventListener("click", (e) => {
+        const childNodes = e.target.parentNode.childNodes;
+        childNodes[childNodes.length-2].innerHTML = e.target.value;
+        // const text = document.qu
+        // document.querySelector("#width-value").innerHTML = "aksjdflajsdf";
+        // console.log(e.target.nextSibling.innerHTML);
+    })
+};
+
+
+
 
 // Add win condition - if all values are > 1 or -3
 // Add lose condition - if clicked value is -2
